51e9496777a04959bfdc4adee27a624bc3202b54
Pavan Kumar Reddy Tadi <tadipavankumarreddy@gmail.com>
2023-05-26 07:39:58 +0530

    Async

--- AndroidJobScheduler/app/src/main/java/in/nareshtech/androidjobscheduler/MainActivity.java
+++ AndroidJobScheduler/app/src/main/java/in/nareshtech/androidjobscheduler/MainActivity.java
@@ -5,23 +5,49 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.job.JobInfo;
 import android.app.job.JobScheduler;
 import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MyReceiver receiver;
     JobScheduler scheduler;
     JobInfo info;
+
+    public static final String DATA_READY_BROADCAST = "someuniquestringhere";
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         scheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+        textView = findViewById(R.id.textView);
 
         info = new JobInfo.Builder(123,new ComponentName(this, MyJobService.class.getName()))
                 .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
                 .build();
+
+        IntentFilter filter = new IntentFilter(DATA_READY_BROADCAST);
+        receiver = new MyReceiver(){
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                super.onReceive(context, intent);
+                String data = intent.getStringExtra("DATA");
+                updateData(data);
+            }
+        };
+
+        registerReceiver(receiver,filter);
+
+    }
+
+    private void updateData(String data) {
+        textView.setText(data);
     }
 
     public void scheduleJob(View view) {
--- AndroidJobScheduler/app/src/main/java/in/nareshtech/androidjobscheduler/MyJobService.java
+++ AndroidJobScheduler/app/src/main/java/in/nareshtech/androidjobscheduler/MyJobService.java
@@ -2,21 +2,72 @@ package in.nareshtech.androidjobscheduler;
 
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.util.Log;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MyJobService extends JobService {
 
-    // We define our task here -> False will be returned when the job is done
-    // -> true will be returned when the task is offloaded to a worker thread
+   private JobParameters jobParameters;
+
     @Override
-    public boolean onStartJob(JobParameters params) {
-        Toast.makeText(this, "Task completed", Toast.LENGTH_SHORT).show();
-        return false;
+    public void onCreate() {
+        super.onCreate();
+        IntentFilter filter = new IntentFilter(MainActivity.DATA_READY_BROADCAST);
+        registerReceiver(new MyReceiver(),filter);
     }
 
-    // We write how to stop the task here
-    // false -> if there is no need of rescheduling a task
-    // true -> when there is a need to reschedule the task
+    @Override
+    public boolean onStartJob(JobParameters params) {
+        this.jobParameters = params;
+        fetchDataFromServer(); // let's say this method handles our background task
+        Toast.makeText(this, "Job Is Running", Toast.LENGTH_SHORT).show();
+        return true;
+    }
+
+    private void fetchDataFromServer() {
+
+        StringBuilder sb = new StringBuilder();
+        Executor e = Executors.newSingleThreadExecutor();
+        e.execute(new Runnable() {
+            @Override
+            public void run() {
+                // This method actually works in the background
+                try {
+                    URL url = new URL("https://api.chucknorris.io/jokes/random");
+                    HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+                    InputStream is = connection.getInputStream();
+                    BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                    String line = "";
+                    while((line = br.readLine())!=null){
+                        sb.append(line);
+                    }
+                    Log.v("MAIN",sb.toString());
+                    jobFinished(jobParameters,false);
+
+                    // Throw a custom broadcast
+                    Intent i = new Intent(MainActivity.DATA_READY_BROADCAST);
+                    i.putExtra("DATA",sb.toString());
+                    sendBroadcast(i);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+
+            }
+        });
+    }
+
     @Override
     public boolean onStopJob(JobParameters params) {
         return false;
